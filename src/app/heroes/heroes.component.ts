4b93ce65a196af2ba4218f596c063772d03fa85f
gauravrajguru <rajgurugaurav77@gmail.com>
2018-07-22 00:03:55 +0530

    Display list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -15,4 +15,9 @@ export class HeroesComponent implements OnInit {
   }
   hero:Hero = {id: 1,name: 'Windstorm'};
   heroes = HEROES;
+  selectedHero:Hero;
+  onSelected(hero:Hero):void{
+    this.selectedHero = hero;
+  }
+
 }
